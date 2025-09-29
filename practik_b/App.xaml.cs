3c8d5ab8a43fcfda36f69cf3cd5222388e4c7586
Admin <Admin@DESKTOP-B6M267U>
2025-09-29 18:04:16 +0300

    Add project files.

---
+++ practik_b/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace practik_b;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ practik_b/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace practik_b;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void Button_Click1(object sender, RoutedEventArgs e)
+    {
+        grid.ItemsSource = Process.GetProcesses();
+    }
+    private void Button_Click2(object sender, RoutedEventArgs e)
+    {
+        Process? pr = grid.SelectedItem as Process;
+        pr?.Kill();
+    }
+    private void Button_Click3(object sender, RoutedEventArgs e)
+    {
+        Process? pr = grid.SelectedItem as Process;
+        string message = "";
+        message += "process name: " + pr?.ProcessName + "\n" + "id: " + pr?.Id + "\n" + "base priority: " + pr?.BasePriority + "\n" + "machine name: " + pr?.MachineName;
+
+        MessageBox.Show(message, "Process info", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+    private void Button_Click4(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show(nameProc.Text);
+        Process.Start(nameProc.Text);
+    }
+}
