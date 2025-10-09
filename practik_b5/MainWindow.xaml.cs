db2f4a8140d6dcf10a70cda2fb5fa10fa0d11cab
Admin <Admin@DESKTOP-B6M267U>
2025-10-09 19:48:21 +0300

    Async await

---
+++ practik_b5/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace practik_b5;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        copyAsync();
+    }
+    void copyAsync()
+    {
+        Task.Run(() =>
+        {
+            try
+            {
+                File.Copy(text1.Text, text2.Text);
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        });
+    }
+}
