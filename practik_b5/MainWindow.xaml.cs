e0f6e8ff9d7b7be5654ad1c65cd670de33001252
Admin <Admin@DESKTOP-B6M267U>
2025-10-12 17:38:17 +0300

    TPL - Task. Parallel

--- practik_b5/MainWindow.xaml.cs
+++ practik_b5/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using PropertyChanged;
+using System.IO;
 using System.Windows;
 
 namespace practik_b5;
@@ -8,26 +10,68 @@ namespace practik_b5;
 /// </summary>
 public partial class MainWindow : Window
 {
+    ViewModel model = new ViewModel();
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = model;
     }
-    private void Button_Click(object sender, RoutedEventArgs e)
+    private void SourceBtn(object sender, RoutedEventArgs e)
     {
-        copyAsync();
+        CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+        if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+        {
+            model.Source = sourceTb.Text = dialog.FileName;
+        }
     }
-    void copyAsync()
+    private void DestBtn(object sender, RoutedEventArgs e)
+    {
+        CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+        dialog.IsFolderPicker = true;
+        if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+        {
+            model.Destination = destTb.Text = dialog.FileName;
+        }
+    }
+    private void CopyBtn(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            string filename = Path.GetFileName(model.Source)!;
+            string destFilename = Path.Combine(model.Destination!, filename);
+            copyFileAsync(model.Source!, destFilename);
+        }
+        catch (Exception x)
+        {
+            MessageBox.Show(x.Message);
+        }
+    }
+    void copyFileAsync(string src, string dest)
     {
         Task.Run(() =>
         {
-            try
+            using FileStream srcStream = new FileStream(src, FileMode.Open, FileAccess.Read);
+            using FileStream desStream = new FileStream(dest, FileMode.Create, FileAccess.Write);
+            byte[] buffer = new byte[1024 * 8];//8Kb
+            int bytes = 0;
+            do
             {
-                File.Copy(text1.Text, text2.Text);
-            }
-            catch (Exception x)
-            {
-                MessageBox.Show(x.Message);
-            }
+                bytes = srcStream.Read(buffer, 0, buffer.Length);
+                desStream.Write(buffer, 0, bytes);
+
+                float percentage = desStream.Length / (srcStream.Length / 100);  
+                model.Progress = percentage;
+
+            } while (bytes > 0);
+            Thread.Sleep(500);
+            model.Progress = 0;
         });
     }
+}
+[AddINotifyPropertyChangedInterface]
+class ViewModel
+{
+    public string? Source { get; set; }
+    public string? Destination { get; set; }
+    public float Progress { get; set; }
 }
